364bc1b4cebb7bc81b264fa5b958053a3b78bebe
Diogo Santos Martins <yogaxpto@gmail.com>
2018-04-25 19:31:20 +0100

    Problem 7

---
+++ src/Problem_7/main.rs
@@ -0,0 +1,12 @@
+extern crate primal;
+fn main() {
+    let mut counter: u16 = 1;
+    let mut x :u64 = 2;
+    while counter <= 10001 {
+        if primal::is_prime(x) {
+            counter+=1;
+        }
+        x+=1;
+    }
+    println!("{}",x-1);
+}
