1f8a523147538a63bc5faad45910349dfe680458
Diogo Santos Martins <yogaxpto@gmail.com>
2018-04-27 22:15:06 +0100

    Problem 10

---
+++ src/Problem_10/main.rs
@@ -0,0 +1,11 @@
+extern crate primal;
+
+fn main() {
+    let mut sum: u64 = 0;
+    for i in 1..2 * 10u64.pow(6) {
+        if primal::is_prime(i) {
+            sum +=i;
+        }
+    }
+    println!("{}", sum);
+}
